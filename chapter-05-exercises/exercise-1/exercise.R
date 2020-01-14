c3f4bee212d13a6ff6dab46304d5bd8c31e8ea27
Caleb Kim <calebkim8@gmail.com>
2020-01-14 14:50:45 -0800

    fix errors

--- chapter-05-exercises/exercise-1/exercise.R
+++ chapter-05-exercises/exercise-1/exercise.R
@@ -1,10 +1,10 @@
 # Exercise 1: practice with basic R syntax
 
 # Create a variable `hometown` that stores the city in which you were born
-hometown <- federalway
+hometown <- "federalway"
 
 # Assign your name to the variable `my_name`
-my_name <- caleb
+my_name <- "caleb"
 
 # Assign your height (in inches) to a variable `my_height`
 my_height <- 1
@@ -16,12 +16,12 @@ puppies <- 1
 puppy_price <- 300
 
 # Create a variable `total_cost` that has the total cost of all of your puppies
-total_cost <- 300
+total_cost <- puppies * puppy_price
 
 # Create a boolean variable `too_expensive`, set to TRUE if the cost is greater 
 # than $1,000
-too_expensive > 1000
+too_expensive <- total_cost > 1000
 
 # Create a variable `max_puppies`, which is the number of puppies you can 
 # afford for $1,000
-max_puppies <- 3
+max_puppies <- 1000%/%puppy_price
